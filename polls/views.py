3ce7ed1b71f008f807a103ea1a6b30b82a38f031
Caroline Rasmussen <cr3730@princeton.edu>
2024-03-16 12:00:29 -0500

    500 error gonegit add .

--- polls/views.py
+++ polls/views.py
@@ -84,5 +84,5 @@ def vote(request, question_id):
     else:
         selected_choice.votes = F("votes") + 1
         selected_choice.save()
-        return JsonResponse({"success": True, "redirect": reverse("polls:results", args=(question.id))})
+        return JsonResponse({"success": True})
     
